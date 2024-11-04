9c846ec96280336d26ab95af90779dca857348cd
ivan <example@gmail.com>
2024-11-04 19:34:38 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// let b = 15;
+// console.log(b);
+// let none = 12;
+// let None = 13;
+// console.log(1)
+// console.log(none);
+// console.log(None);
+
+// const obj = {
+//     a: 50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// let vlad = (1 / 0);
+// console.log(vlad)
+// let kalik;
+// console.log(kalik)
+
+
+// let personObj = null
+
+// personObj = {
+//   name: 'Вася',
+//   age: 25,
+// }
+// console.log(personObj)
+// // let eshkerya = 24; (const)
+// // eshkerya = 12;
+// // console.log(eshkerya)
+
+// const info = {
+//     name: "karina",
+//     age: 26,
+//     isMarried: false
+// }
+// console.log(info.isMarried);
+// console.log(info ["age"]);
+
+
+// let arr = ["first", 6, "third", "fourth"];
+// console.log(arr[4]);
+
+
+
+//alert prompt confirm//
+
+// alert("hello world");
+// const anw = +prompt("hey", [123]);
+// console.log(anw + 5)
+// console.log(typeof(anw));
+// // console.log(typeof(+anw));
+// confirm("u gay");
+// const ress = confirm("kringe");
+// console.log(ress)
+// const answersss = [];
+// answersss[0] = prompt("сколько вам лет?", "");
+// answersss[1] = prompt("как вас зовут", "");
+// answersss[2] = prompt("база?", "");
+
+// document.write(answersss);
+// console.log(typeof(null)) //объект
+
+
+
+
+//интерполяция
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'alex';
+// alert(`привет, ${user}`);
+
+
+
+
+//операторы 
+console.log('arr' + ' objerct');
+console.log(4 + "5");
+console.log(4 + +"5");
+
+let incr = 10,
+decr = 10;
+// incr++;
+// decr--;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(decr--);
+
+// console.log(5%2);
+// console.log(4*2 == '8');
+// console.log(4*2 == '9');
+// console.log(4*2 ==='8');
+
+// && = 2
+// || = 1
+const isChecked = false,
+isClose = true;
+
+console.log(!isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(isChecked || !isClose);
+console.log(2 + 2 * 2 != 10);
+console.log(2 + 2 * 2 !== '6');
+
+
